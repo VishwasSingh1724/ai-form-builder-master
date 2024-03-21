b86160ac7b1a5f63cc87dc15afa3af025bfd7e25
Vishwas Singh <vishwass440@gmail.com>
2024-03-21 20:11:56 +0530

    initial deployement commit

---
+++ src/app/(admin)/analytics/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Analytics = () => {
+  return (
+    <div>Analytics page will be added soon belive me 😉</div>
+  )
+}
+
+export default Analytics
---
+++ src/app/(admin)/charts/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Charts = () => {
+  return (
+    <div>Charts page will be added soon as well belive me</div>
+  )
+}
+
+export default Charts
